4b8b7dddf5462f7f20fc65c2d275a2a0321e3b02
Fatima-137 <58041411+Fatima-137@users.noreply.github.com>
2020-07-11 20:08:33 +0500

    Stop Watch

--- app.js
+++ app.js
@@ -8,40 +8,28 @@ var interval;
 
 
 function timer(){
-//     countdown = countdown++ >= 100 ? 0 : countdown;
-
-//   countdownNumberEl.textContent = countdown;
-    miliSeconds = miliSeconds++ >= 100 ? 0 : miliSeconds;
-    // miliSeconds.innerHTML = miliSeconds;
-    miliSecondsHeading.textContent = miliSeconds;
+    miliSeconds++;
+    miliSeconds.innerHTML = miliSeconds;
     if(miliSeconds >= 100){
         seconds++;
         secondsHeading.innerHTML = seconds;
         miliSeconds = 0;
-//         countdown = countdown++ >= 100 ? 0 : countdown;
-
-//   countdownNumberEl.textContent = countdown;
     }
     else if(seconds >= 60){
         minutes++;
         minutesHeading.innerHTML = minutes;
         seconds = 0;
-//         countdown = countdown++ >= 60 ? 0 : countdown;
-
-//   countdownNumberEl.textContent = countdown;
     }
 }
 
 function start(){
     interval = setInterval(timer, 10);
     document.getElementById('start').disabled = true;
-    // toggle();
 }
 
 function pause(){
     clearInterval(interval);
     document.getElementById('start').disabled = false;
-    // toggle();
 }
 function reset(){
     minutes = 0;
@@ -52,54 +40,4 @@ function reset(){
     miliSecondsHeading.innerHTML = miliSeconds;
     pause();
     document.getElementById('start').disabled = false;
-    // if (reset()==1){
-    //     var a = document.getElementById("reset").disable.false;
-    // }
-    // else{
-    //     a = disable.false;
-    // }
 }
-// function disableNextButton(start){
-    // document.getElementById('start').disabled = true;
-// }
-
-// var start = document.getElementById("start");
-// var pause = document.getElementById("pause");
-// function toggle(){
-//     if(start.disabled){
-//         start.disabled = false;
-//         pause.disabled = true;
-//     }
-//     else{
-//         start.disabled = true;
-//         pause.disabled = false;
-//     }
-// }
-
-// start.addEventListener("click", function(){
-//     toggle();
-//     interval = setInterval(count, 1000);
-// });
-/* * The setTimeout({},0) is a workaround for what appears to be a bug in StackSnippets. * It should not be required. See JSFiddle version. */ 
-// setTimeout(function() { 
-//     var time = 10; 
-    // / how long the timer will run (seconds) / 
-    // var initialOffset = '440'; 
-    // var i = 1;
-    //  / Need initial run as interval hasn't yet occured... / 
-    // $('.circle_animation').css('stroke-dashoffset', initialOffset-(1(initialOffset/time))); 
-    // var interval = setInterval(function() { 
-        // $('text').text(i); 
-        // if (i == time) { 	 
-            // clearInterval(interval); return; } 
-// $('.circle_animation').css('stroke-dashoffset', initialOffset-((i+1)(initialOffset/time))); i++; }, 1000); }, 0);
-// var countdownNumberEl = document.getElementById('countNum');
-// var countdown = 0;
-
-// countdownNumberEl.textContent = countdown;
-
-// interval = setInterval(function() {
-//   countdown = countdown++ >= 10 ? 0 : countdown;
-
-//   countdownNumberEl.textContent = countdown;
-// }, 10);
